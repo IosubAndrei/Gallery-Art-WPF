dcc8f8b3f03079d1d8b3bf00251534f9cb30b372
Roiul <roiurazvan2000@gmail.com>
2021-11-11 20:05:32 +0200

    "Begin functionalities"

--- BD_Proiect/BD_Proiect/Register.xaml.cs
+++ BD_Proiect/BD_Proiect/Register.xaml.cs
@@ -11,12 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BD_Proiect
 {
-    /// <summary>
-    /// Interaction logic for Register.xaml
-    /// </summary>
     public partial class Register : Window
     {
         public Register()
@@ -24,14 +23,18 @@ namespace BD_Proiect
             InitializeComponent();
         }
 
+        SqlConnection con = new SqlConnection("Server=.;Database=BD_Proiect;Trusted_Connection=true");
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(txtUsername.Text == "" && txtPassword.Text == "" && txtConfirmPassword.Text=="")
-            {
+            if (txtUsername.Text == "" && txtPassword.Text == "" && txtConfirmPassword.Text == "")
                 MessageBox.Show("Username and Password fields are empty!", "Sign Up Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (txtPassword.Text == txtConfirmPassword.Text)
+            {
+                con.Open();
+                string register = "INSERT INTO Users VALUES ('" + txtUsername.Text + "','" + txtPassword.Text + "')";
+                con.Close();
             }
-            else if()
-            { }
         }
     }
 }
