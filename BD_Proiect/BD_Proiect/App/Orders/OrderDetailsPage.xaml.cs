a31aaf375f2d0205653e542216d89cc4eb5fdb9a
Roiul <roiurazvan2000@gmail.com>
2021-11-18 19:49:29 +0200

    little edit

--- BD_Proiect/BD_Proiect/App/Orders/OrderDetailsPage.xaml.cs
+++ BD_Proiect/BD_Proiect/App/Orders/OrderDetailsPage.xaml.cs
@@ -12,19 +12,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace BD_Proiect.Orders
 {
-    /// <summary>
-    /// Interaction logic for OrderDetailsPage.xaml
-    /// </summary>
     public partial class OrderDetailsPage : UserControl
     {
+        static string connectionString = "Server=.;Database=BD_Proiect;Trusted_Connection=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+        DataSet DS = new DataSet();
+        SqlDataAdapter DA = new SqlDataAdapter();
+
         public Action acceptOrder;
         public Action<int> declineOrder;
         public OrderDetailsPage(int userID,int operaID)
         {
             InitializeComponent();
+
+            SqlCommand clientiCMD = new SqlCommand();
+            SqlCommand comenziCMD = new SqlCommand();
+
+            connection.Open();
+
+            string nume = numeTextBox.Text;
+            string prenume = prenumeTextBox.Text;
+            string phone = telefonTextBox.Text;
+            string address = addressTextBox.Text;
+            string loc = locTextBox.Text;
+
+            clientiCMD.CommandText = "INSERT INTO Clienti VALUES ('"+nume+"', '"+prenume+"','"+phone+"','"+address+"','"+loc+"')";
         }
 
         private void declineButton_Click(object sender, RoutedEventArgs e)
