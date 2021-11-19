58ae7b203989a7dfa6ffd1e5de9754f6410929e8
IosubAndrei <iosubandrei2010@yahoo.com>
2021-11-19 11:41:09 +0200

    Added Images for OrdersDetails

--- BD_Proiect/BD_Proiect/App/Orders/OrderDetailsPage.xaml.cs
+++ BD_Proiect/BD_Proiect/App/Orders/OrderDetailsPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +25,40 @@ namespace BD_Proiect.Orders
     {
         public Action acceptOrder;
         public Action<int> declineOrder;
+
+        static string connectionString = "Server=.;Database=BD_Proiect;Trusted_Connection=true";
+        SqlConnection connection = new SqlConnection(connectionString);
+        DataSet DS = new DataSet();
+        SqlDataAdapter DA = new SqlDataAdapter();
         public OrderDetailsPage(int userID,int operaID)
         {
+            SqlCommand CMD = new SqlCommand();
+
             InitializeComponent();
+
+            connection.Open();
+
+            CMD.Connection = connection;
+            CMD.CommandText = "SELECT O.ImageURL FROM Opere_De_Arta AS O WHERE O.ID_Opera=@IDOpera";
+            CMD.Parameters.AddWithValue("@IDOpera", operaID);
+
+            DbDataReader db = CMD.ExecuteReader();
+            while (db.Read())
+            {
+                ImageURL = db.GetValue(0).ToString();
+            }
+
+            connection.Close();
+
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(ImageURL, UriKind.Absolute);
+            bitmap.EndInit();
+
+            ImageGrid.Source = bitmap;
         }
+
+        public string ImageURL { get; set; }
 
         private void declineButton_Click(object sender, RoutedEventArgs e)
         {
--- BD_Proiect/BD_Proiect/Login/Login.xaml.cs
+++ BD_Proiect/BD_Proiect/Login/Login.xaml.cs
@@ -53,8 +53,6 @@ namespace BD_Proiect
         private void checkbxShowPassword_Checked(object sender, RoutedEventArgs e)
         {
             txtPassword.Text = passwordBox.Password;
-            passwordBox.Password = "admin";
-            txtUsername.Text = "admin";
             passwordBox.Visibility = System.Windows.Visibility.Collapsed;
             txtPassword.Visibility = System.Windows.Visibility.Visible;
 
